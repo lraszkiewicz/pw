a46c08e4b961664ffb131c6f522489d575985673
Łukasz Raszkiewicz <lraszkiewicz@gmail.com>
2017-01-23 21:33:24 +0100

    a

--- circuit/parse_input.c
+++ circuit/parse_input.c
@@ -7,157 +7,152 @@
 #include "./parse_input.h"
 
 void readFirstLine(long* n, long* k, long* v) {
-    scanf("%ld%ld%ld", n, k, v);
+  scanf("%ld%ld%ld", n, k, v);
 }
 
 Node* createNode() {
-    Node* node = malloc(sizeof(Node));
-    node->var = -1;
-    node->num = -1;
-    node->parents = NULL;
-    node->children = NULL;
-    node->pipesToChildren = NULL;
-    node->pipesToParents = NULL;
-    node->pipeToMain[0] = -1;
-    node->pipeToMain[1] = -1;
+  Node* node = malloc(sizeof(Node));
+  node->var = -1;
+  node->num = -1;
+  node->parents = NULL;
+  node->children = NULL;
+  node->pipesToChildren = NULL;
+  node->pipesToParents = NULL;
+  node->pipeToMain[0] = -1;
+  node->pipeToMain[1] = -1;
 }
 
 void insertNode(Node* node, NodeList** list) {
-    NodeList* newList = malloc(sizeof(NodeList));
-    newList->node = node;
-    newList->next = *list;
-    *list = newList;
+  NodeList* newList = malloc(sizeof(NodeList));
+  newList->node = node;
+  newList->next = *list;
+  *list = newList;
 }
 
 Node* parseNode(char* s, long b, long e, Node** x, Node* currentNode) {
-    Node* node;
-    if (currentNode == NULL)
-        node = createNode();
-    else
-        node = currentNode;
+  Node* node;
+  if (currentNode == NULL)
+    node = createNode();
+  else
+    node = currentNode;
 
-    while (isspace(s[b]))
-        ++b;
-    while (isspace(s[e]))
-        --e;
-    while (s[b] == '(' && s[e] == ')') {
-        ++b;
-        --e;
-    }
+  while (isspace(s[b]))
+    ++b;
+  while (isspace(s[e]))
+    --e;
+  while (s[b] == '(' && s[e] == ')') {
+    ++b;
+    --e;
+  }
 
-    // check if SUM or MULTIPLY
-    long openBrackets = 0;
-    long mid = -1;
-    bool endFor = false;
-    for (long i = b; i <= e && !endFor; ++i) {
-        switch (s[i]) {
-            case '(':
-                ++openBrackets;
-                break;
-            case ')':
-                --openBrackets;
-                break;
-            case '+':
-                if (openBrackets == 0) {
-                    node->type = SUM;
-                    mid = i;
-                    endFor = true;
-                }
-                break;
-            case '*':
-                if (openBrackets == 0) {
-                    node->type = MULTIPLY;
-                    mid = i;
-                    endFor = true;
-                }
-                break;
-            default:
-                continue;
+  // check if SUM or MULTIPLY
+  long openBrackets = 0;
+  long mid = -1;
+  bool endFor = false;
+  for (long i = b; i <= e && !endFor; ++i) {
+    switch (s[i]) {
+      case '(':
+        ++openBrackets;
+        break;
+      case ')':
+        --openBrackets;
+        break;
+      case '+':
+        if (openBrackets == 0) {
+          node->type = SUM;
+          mid = i;
+          endFor = true;
         }
+        break;
+      case '*':
+        if (openBrackets == 0) {
+          node->type = MULTIPLY;
+          mid = i;
+          endFor = true;
+        }
+        break;
+      default:
+        continue;
     }
-    if (endFor) {
-        insertNode(parseNode(s, mid + 1, e, x, NULL), &node->parents);
-        insertNode(node, &node->parents->node->children);
-        insertNode(parseNode(s, b, mid - 1, x, NULL), &node->parents);
-        insertNode(node, &node->parents->node->children);
-        return node;
-    }
+  }
+  if (endFor) {
+    insertNode(parseNode(s, mid + 1, e, x, NULL), &node->parents);
+    insertNode(node, &node->parents->node->children);
+    insertNode(parseNode(s, b, mid - 1, x, NULL), &node->parents);
+    insertNode(node, &node->parents->node->children);
+    return node;
+  }
 
-    // check if MINUS
-    if (s[b] == '-') {
-        node->type = MINUS;
-        insertNode(parseNode(s, b + 1, e, x, NULL), &node->parents);
-        insertNode(node, &node->parents->node->children);
-        return node;
-    }
+  // check if MINUS
+  if (s[b] == '-') {
+    node->type = MINUS;
+    insertNode(parseNode(s, b + 1, e, x, NULL), &node->parents);
+    insertNode(node, &node->parents->node->children);
+    return node;
+  }
 
-    // check if PNUM
-    if ('0' <= s[b] && s[b] <= '9') {
-        node->type = PNUM;
-        sscanf(s + b, "%ld", &node->num);
-        return node;
-    }
+  // check if PNUM
+  if ('0' <= s[b] && s[b] <= '9') {
+    node->type = PNUM;
+    sscanf(s + b, "%ld", &node->num);
+    return node;
+  }
 
-    // check if VAR
-    if (s[b] == 'x') {
-        free(node);
-        long var;
-        sscanf(s + b, "x[%ld]", &var);
-        return x[var];
-    }
-
+  // check if VAR
+  if (s[b] == 'x') {
     free(node);
-    return NULL;
+    long var;
+    sscanf(s + b, "x[%ld]", &var);
+    return x[var];
+  }
+
+  free(node);
+  return NULL;
 }
 
 // false if error
 bool readEquation(Node** x) {
-    char s[1000];
-    long var;
-    scanf("%*d x[%ld] = %[^\n]s", &var, s);
-    if (x[var]->parents != NULL)
-        return false;
+  char s[1000];
+  long var;
+  scanf("%*d x[%ld] = %[^\n]s", &var, s);
+  if (x[var]->parents != NULL)
+    return false;
 
-    parseNode(s, 0, (strlen(s)) - 1, x, x[var]);
-//    Node* node = parseNode(s, 0, (strlen(s)) - 1, x, x[var]);
-//    node->children = x[var]->children;
-//    free(x[var]);
-//    x[var] = node;
-//    node->var = var;
-    return true;
+  parseNode(s, 0, (strlen(s)) - 1, x, x[var]);
+  return true;
 }
 
 InitializerList* parseInitializerList(char* s) {
-    long var, num;
-    if (sscanf(s, " x[%ld] %ld", &var, &num) >= 2) {
-        int xs = 0;
-        int i;
-        for (i = 0; s[i] != 0 && s[i] != '\n'; ++i) {
-            if (s[i] == 'x')
-                ++xs;
-            if (xs == 2)
-                break;
-        }
-        InitializerList* initializerList = malloc(sizeof(InitializerList));
-        initializerList->var = var;
-        initializerList->num = num;
-        initializerList->next = parseInitializerList(s + i);
-        return initializerList;
-    } else {
-        return NULL;
+  long var, num;
+  if (sscanf(s, " x[%ld] %ld", &var, &num) >= 2) {
+    int xs = 0;
+    int i;
+    for (i = 0; s[i] != 0 && s[i] != '\n'; ++i) {
+      if (s[i] == 'x')
+        ++xs;
+      if (xs == 2)
+        break;
     }
+    InitializerList* initializerList = malloc(sizeof(InitializerList));
+    initializerList->var = var;
+    initializerList->num = num;
+    initializerList->next = parseInitializerList(s + i);
+    return initializerList;
+  } else {
+    return NULL;
+  }
 }
 
 InitializerList* readInitializerList() {
-    char s[1000];
-    scanf(" %[^\n]s", s);
-    long len = strlen(s);
-    bool wasDigit = false;
-    long i;
-    for (i = 0; i < len; ++i)
-        if ('0' <= s[i] && s[i] <= '9')
-            wasDigit = true;
-        else if (wasDigit && isspace(s[i]))
-            break;
-    return parseInitializerList(s + i);
+  char s[1000];
+  scanf(" %[^\n]s", s);
+  long len = strlen(s);
+  bool wasDigit = false;
+  long i;
+  for (i = 0; i < len; ++i)
+    if ('0' <= s[i] && s[i] <= '9')
+      wasDigit = true;
+    else if (wasDigit && isspace(s[i]))
+      break;
+  return parseInitializerList(s + i);
 }
--- circuit/parse_input.h
+++ circuit/parse_input.h
@@ -2,40 +2,40 @@
 #define CIRCUIT_PARSE_INPUT_H
 
 typedef enum NodeType {
-    SUM,
-    MULTIPLY,
-    MINUS,
-    PNUM,
-    VAR
+  SUM,
+  MULTIPLY,
+  MINUS,
+  PNUM,
+  VAR
 } NodeType;
 
 typedef struct NodeList NodeList;
 
 typedef struct PipeList {
-    int fd[2];
-    struct PipeList* next;
+  int fd[2];
+  struct PipeList* next;
 } PipeList;
 
 typedef struct Node {
-    long num; // used only if type is PNUM
-    long var; // i from x[i] if applies, -1 in other cases
-    NodeType type;
-    NodeList* parents;
-    NodeList* children;
-    PipeList* pipesToParents;
-    PipeList* pipesToChildren;
-    int pipeToMain[2];
+  long num; // used only if type is PNUM
+  long var; // i from x[i] if applies, -1 in other cases
+  NodeType type;
+  NodeList* parents;
+  NodeList* children;
+  PipeList* pipesToParents;
+  PipeList* pipesToChildren;
+  int pipeToMain[2];
 } Node;
 
 typedef struct NodeList {
-    Node* node;
-    struct NodeList* next;
+  Node* node;
+  struct NodeList* next;
 } NodeList;
 
 typedef struct InitializerList {
-    long var;
-    long num;
-    struct InitializerList* next;
+  long var;
+  long num;
+  struct InitializerList* next;
 } InitializerList;
 
 Node* createNode();
